5670b53b0c741899edb898e900801f5c41db060b
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2025-03-12 12:11:15 -0400

    BC + adaptive stepsize + transient source

--- analysis/festim_model.py
+++ analysis/festim_model.py
@@ -1,5 +1,7 @@
 import festim as F
 from openmc2dolfinx import UnstructuredMeshReader
+import dolfinx
+import numpy as np
 
 
 # NOTE need to override these methods in ParticleSource until a
@@ -11,7 +13,13 @@ class ParticleSourceFromOpenMC(F.ParticleSource):
 
     @property
     def time_dependent(self):
-        return False
+        return True
+
+    def update(self, t):
+        if t < 12 * 3600:
+            return
+        else:
+            self.value_fenics.x.array[:] = 0.0
 
 
 # convert openmc result
@@ -25,40 +33,64 @@ dolfinx_mesh.geometry.x[:] /= 100
 tritium_source_term = reader.create_dolfinx_function("mean")
 
 neutron_rate = 1e8  # n/s
-tritium_source_term.x.array[:] *= neutron_rate
+percm3_to_perm3 = 1e6
+tritium_source_term.x.array[:] *= neutron_rate * percm3_to_perm3
 
 my_model = F.HydrogenTransportProblem()
 
 my_model.mesh = F.Mesh(mesh=reader.dolfinx_mesh)
 
+
+class TopSurface(F.SurfaceSubdomain):
+    def locate_boundary_facet_indices(self, mesh):
+        z_top = mesh.geometry.x[:, 2].max()
+        return dolfinx.mesh.locate_entities_boundary(
+            mesh, mesh.topology.dim - 1, lambda x: np.isclose(x[2], z_top)
+        )
+
+
 T = F.Species("T")
 my_model.species = [T]
 
-salt = F.Material(D_0=1, E_D=0)
+salt = F.Material(D_0=1e-8, E_D=0.42)
 volume = F.VolumeSubdomain(id=1, material=salt)
+top_boundary = TopSurface(id=2)
 
-my_model.subdomains = [volume]
+my_model.subdomains = [volume, top_boundary]
 
 my_model.sources = [
     ParticleSourceFromOpenMC(value=tritium_source_term, volume=volume, species=T),
 ]
 
+my_model.boundary_conditions = [
+    F.FixedConcentrationBC(subdomain=top_boundary, species=T, value=0.0)
+]
+
 my_model.temperature = 650 + 273.15  # 650 degC
+
+dt = F.Stepsize(
+    3600,
+    growth_factor=1.2,
+    cutback_factor=0.9,
+    target_nb_iterations=4,
+    milestones=[12 * 3600],
+)
 
 my_model.settings = F.Settings(
     atol=1e-10,
-    rtol=1e-10,
-    final_time=3600,
-    stepsize=100,
+    rtol=1e-20,
+    final_time=60 * 24 * 3600,
+    stepsize=dt,
 )
 
 my_model.exports = [
     F.VTXSpeciesExport("tritium_concentration.bp", field=T),
 ]
 
-# from dolfinx.log import set_log_level, LogLevel
+from dolfinx.log import set_log_level, LogLevel
 
 # set_log_level(LogLevel.INFO)
 
 my_model.initialise()
+
 my_model.run()
